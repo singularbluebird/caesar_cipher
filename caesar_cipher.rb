104949856d5bcd927e440cc22b57a7ab88b86d67
Samuel <samysarria.ss@gmail.com>
2025-04-02 12:27:49 -0300

    Changing all files to adhere to ruby's guidelines

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,19 +1,19 @@
 require 'pry-byebug'
 
 def caesar_cipher(word, num)
-  ciphered = [] 
+  ciphered = []
   word.each_char do |c|
-    if c.ord >= 65 && c.ord <= 90 
+    if c.ord >= 65 && c.ord <= 90
       letter = ((((c.ord - 65) + num) % 26) + 65).chr
       ciphered << letter
     elsif c.ord >= 97 && c.ord <= 122
       letter = ((((c.ord - 97) + num) % 26) + 97).chr
       ciphered << letter
-    else 
+    else
       ciphered << c
     end
   end
-  ciphered.join()
+  ciphered.join
 end
 
 puts 'What a string!'
